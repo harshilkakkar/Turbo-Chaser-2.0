7470b3cd97ee5d0d1a33d63de641648c038debe2
Harshil Kakkar <kakkar.harshil@gmail.com>
2021-02-21 20:02:06 +1100

    Reset button

--- js/Form.js
+++ js/Form.js
@@ -3,6 +3,7 @@ class Form {
   constructor() {
     this.input = createInput("Name");
     this.button = createButton('Play');
+    this.resetButton= createButton('Reset');
     this.greeting = createElement('h2');
     this.title = createElement('h2');
   }
@@ -19,7 +20,8 @@ class Form {
 
     this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
     this.button.position(displayWidth/2 + 30, displayHeight/2);
-2
+    
+
     this.button.mousePressed(()=>{
       this.input.hide();
       this.button.hide();
@@ -32,5 +34,13 @@ class Form {
       this.greeting.position(displayWidth/2 - 70, displayHeight/4);
     });
 
+    this.resetButton.position(displayWidth - 100, 30);
+    this.resetButton.mousePressed(()=>{
+    player.updateCount(0);
+    game.update(0);
+    player.update(0);
+  })
+
   }
+  
 }
--- js/Player.js
+++ js/Player.js
@@ -23,6 +23,7 @@ class Player {
     database.ref(playerIndex).set({
       name:this.name,
       distance:this.distance
+    
     });
   }
 
